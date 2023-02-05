e5b67476e8db5d5d9f903e7ba165bf87cb91fbd6
Saurabh Mahajan <saurabh.mahajan@coupa.com>
2023-02-05 17:08:56 +0530

    First Commit

---
+++ HelloDocker/HelloDocker.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace HelloDocker.Console;
+
+public class Program
+{
+    private static string _connectionString;
+    private static string _clientId;
+
+    public static void Main(string[] args)
+    {
+        _connectionString = Environment.GetEnvironmentVariable("DB_CONN");
+        _clientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+
+        if (string.IsNullOrEmpty(_connectionString))
+            _connectionString =
+                "Server=tcp:algo-db-server.database.windows.net,1433;Database=algo-db;User ID=algo_user;Password=$Pr0f!7@2022@!#;MultipleActiveResultSets=true;";
+
+        if (string.IsNullOrEmpty(_clientId)) _clientId = "A12345";
+
+        for (int i = 0; i < 5; i++)
+        {
+            LogToDatabase(i);
+            Thread.Sleep(10000);
+        }
+    }
+
+    private static void LogToDatabase(int i)
+    {
+        using (var con = new SqlConnection(_connectionString))
+        {
+            var cmd = con.CreateCommand();
+            cmd.CommandText = "INSERT INTO RunTest (Message, TimeStamp) VALUES (@Message, @TimeStamp)";
+            cmd.Parameters.AddWithValue("@Message", $"{_clientId} run count is {i}");
+            cmd.Parameters.AddWithValue("@TimeStamp", DateTime.Now);
+            con.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+az acr create --resource-group demo --name saurabhacidemo --sku basic
+
+az acr login --name saurabhacidemo
+
+docker images
+
+docker tag 30a2d6c8f7b7 saurabhacidemo.azurecr.io/hellodockerconsole:v1
+
+docker push saurabhacidemo.azurecr.io/hellodockerconsole
+
+$REGISTERY_NAME="saurabhacidemo"
+
+$ACR_REGISTRY_ID = az acr show --name saurabhacidemo --query id --output tsv
+
+$ACR_REGISTRY_ID = az acr show --name saurabhacidemo --query id --output tsv
+
+$SP_PASSWORD = az ad sp create-for-rbac --name acr-demo-pull --scopes $ACR_REGISTRY_ID --role acrpull --query password --output tsv
+
+$SP_APP_ID = az ad sp list --display-name acr-demo-pull --query [].appId --output tsv
+
+az container create --resource-group demo --name democontainer --image saurabhacidemo.azurecr.io/hellodockerconsole:v1 --registry-username $SP_APP_ID --registry-password $SP_PASSWORD --dns-name aci-demo-2222 --port 80
+
+az acr repository list --name saurabhacidemo
+
+---
+
+ENV Variables
+
+az container create --resource-group demo --name democontainerv3 --image saurabhacidemo.azurecr.io/hellodockerconsole:v2 --registry-username $SP_APP_ID --registry-password $SP_PASSWORD --dns-name aci-demo-444 --port 80 --environment-variables CLIENT_ID=ZZZ123
